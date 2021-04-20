c4070e76ceafe4e40abea3de58131f3ddf9f9884
Matías Vergara S <matiasvergara@ug.uchile.cl>
2021-04-19 22:35:38 -0400

    Ejercicio 2 terminado

--- Ejercicio 2/cgi-bin/save_doctor.py
+++ Ejercicio 2/cgi-bin/save_doctor.py
@@ -22,24 +22,22 @@ head = """
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta charset="utf-8" /> <!-- Declaring enconding as UTF 8-->
     <title> Ejercicio 3</title> <!-- Title in pestaña -->
-    <link rel="stylesheet" type="text/css" media="screen"  href="style.css" />    <!-- CSS: -->
+    <link rel="stylesheet" type="text/css" media="screen"  href="../style.css" />    <!-- CSS: -->
 </head>
 """
 
-body = """
+body_open = """
 <ul class="topnav">
-  <li><a class="active" href="index.html">Inicio</a></li>
-  <li><a href="add_doctor.html">Agregar Datos de Médico</a></li>
+  <li><a class="active" href="../index.html">Inicio</a></li>
+  <li><a href="../add_doctor.html">Agregar Datos de Médico</a></li>
 </ul>
 
 <div style="padding:0 16px;">
-  <h2>Ayuda médica</h2>
-  <p>En un mundo donde el coronavirus ha arruinado la estabilidad de los gobiernos, surge una página con gran propósito:</p>
-  <p>Salvar a la humanidad.</p>
-</div>
+  <h2>Datos recibidos con éxito</h2>
 
-<div style="padding:0 16px;">
-  <h3>Créditos Alonso Utreras</h3>
+"""
+
+body_close = """
 </div>
 </body>
 """
@@ -51,9 +49,12 @@ html_close = """
 form = cgi.FieldStorage()
 print(html_open, file=utf8stdout)
 print(head, file=utf8stdout)
-print(body, file=utf8stdout)
+print(body_open, file=utf8stdout)
+
+renamed_keys = {"nombre-medico": "Nombre del médico", "celular-medico": "Número celular", "experiencia-medico": "Experiencia",
+                "especialidad-medico": "Especialidad", "email-medico": "Dirección de correo electrónico"}
+for key in form.keys():
+    print("<p>" + renamed_keys[key] + ": " + form[key].value + "</p>", file=utf8stdout)
+print(body_close, file=utf8stdout)
 print(html_close, file=utf8stdout)
 
-for key in form.keys():
-    print("<p>" + form[key].value + "</p>", file=utf8stdout)
-print(footer, file=utf8stdout)
